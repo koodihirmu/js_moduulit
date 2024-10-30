4299b90534b8a28692f156b1ddb9174d0fe035e3
koodihirmu <tomvar98@gmail.com>
2024-10-30 17:19:23 +0200

    tv app error handling made better

--- moduuli_4/4_3_tv_app_v3/js/script.js
+++ moduuli_4/4_3_tv_app_v3/js/script.js
@@ -20,33 +20,40 @@ if (form) {
 			const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query.value}`)
 			const jsonData = await response.json()
 			// assign data asynchronously
-			jsonData.forEach(async (item) => {
-				// create card element
-				const card = document.createElement("article")
-				card.className = "card"
+			if (jsonData && jsonData.length > 0) {
+				jsonData.forEach(async (item) => {
+					// create card element
+					const card = document.createElement("article")
+					card.className = "card"
 
-				// create title
-				const h2 = document.createElement("h2")
-				h2.innerText = item["show"]["name"]
+					// create title
+					const h2 = document.createElement("h2")
+					h2.innerText = item["show"]["name"]
 
-				// create link
-				const a = document.createElement("a")
-				a.href = item["show"]["url"]
-				a.innerText = item.show.url
-				a.target = "_blank"
+					// create link
+					const a = document.createElement("a")
+					a.href = item["show"]["url"]
+					a.innerText = item.show.url
+					a.target = "_blank"
 
-				// create img
-				const img = document.createElement("img")
-				// I love anonymous functions
-				img.src = (() => { return item.show.image?.medium || "https://placehold.co/210x295" })()
-				// Though this way would work too
-				//img.src = item.show.image?.medium || "https://placehold.co/210x295"
+					// create img
+					const img = document.createElement("img")
+					// I love anonymous functions
+					img.src = (() => { return item.show.image?.medium || "https://placehold.co/210x295" })()
+					// Though this way would work too
+					//img.src = item.show.image?.medium || "https://placehold.co/210x295"
 
-				card.appendChild(h2)
-				card.appendChild(a)
-				card.appendChild(img)
-				section.appendChild(card)
-			})
+					card.appendChild(h2)
+					card.appendChild(a)
+					card.appendChild(img)
+					section.appendChild(card)
+				})
+			} else {
+				let p = document.createElement("p")
+				p.innerText = "No results!"
+				section.appendChild(p)
+			}
+
 		}
 		catch (error) {
 			console.log(error.message)
